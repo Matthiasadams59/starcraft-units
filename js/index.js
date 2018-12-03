6e8625f10cf005bb218df458dd0be8ba0b283e75
William <william.aboucaya@gmail.com>
2018-12-03 22:37:09 +0100

    Added sound assets for units creation and deletion

--- js/index.js
+++ js/index.js
@@ -12,18 +12,23 @@ const addUnit = () => {
 	const name = document.getElementById("unitNameField").value
 	const type = document.getElementById("unitTypeField").value
 	const unit = null
+	const audio = null
 	switch (type) {
 		case "Marine":
 			unit = new Marine(name, idCounter)
+			audio = new Audio('../resources/sound/MarineReady.oga');
 			break
 		case "Siege Tank":
 			const siegeMode = document.getElementById("unitSiegeMode")
 			unit = new SiegeTank(name, idCounter, siegeMode)
+			audio = new Audio('../resources/sound/SiegeTankReady.oga');
 			break
 		case "Valkyrie":
 			unit = new Valkyrie(name, idCounter)
+			audio = new Audio('../resources/sound/ValkyrieReady.oga');
 			break
 	}
+	audio.play();
 	battlefield.add(unit)
 	idCounter++
 	return unit
